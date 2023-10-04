cc307b3c7b469127a4e23b4bd321359ca1930c75
imAliAzhar <ali.azhar@finn.auto>
2023-10-04 14:38:09 +0500

    Update alfred script to list cached when arg empty

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,34 +1,41 @@
 query=$1
 
-paths=(
+search_dirs=(
     ~/
     ~/Documents
     ~/.config
 )
 
 search_path_params=""
-for path in $paths; do
-    search_path_params+="--search-path $path "
+for dir in $search_dirs; do
+    search_path_params+="--search-path $dir "
 done
 
-/opt/homebrew/bin/fd -t d -t f --max-results 10 ${=search_path_params} -g "*$query*" -0 |
-    /usr/bin/xargs -0 /$HOME/.local/bin/mru sort |
-    /opt/homebrew/bin/jq -nR '{
-        "items": [
-            inputs |
-            rtrimstr("/") as $path |
-            $path |
-            split("/") |
-            last as $name |
-            {
-                "uid": $path,
-                "title": $name,
-                "arg": $path,
-                "match": $name,
-                "icon": {
-                    type: "fileicon",
-                    path: $path
-                }
+
+if [ -z "$query" ]
+then
+    paths=$($HOME/.local/bin/mru list)
+else
+    paths=$(/opt/homebrew/bin/fd -t d -t f --max-results 10 ${=search_path_params} -g "*$query*" -0 |
+        /usr/bin/xargs -0 $HOME/.local/bin/mru sort)
+fi
+
+echo $paths | /opt/homebrew/bin/jq -nR '{
+    "items": [
+        inputs |
+        rtrimstr("/") as $path |
+        $path |
+        split("/") |
+        last as $name |
+        {
+            "uid": $path,
+            "title": $name,
+            "arg": $path,
+            "match": $name,
+            "icon": {
+                type: "fileicon",
+                path: $path
             }
-        ]
-    }'
+        }
+    ]
+}'
